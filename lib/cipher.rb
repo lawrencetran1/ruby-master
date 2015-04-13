7c122943f21f643f7000f1abfe66d59c1068cd59
Lawrence Tran <lawrencetran1@gmail.com>
2015-04-13 10:48:38 -0700

    Completed challenge

--- lib/cipher.rb
+++ lib/cipher.rb
@@ -1,3 +1,21 @@
 class Cipher
+	def self.encrypt(word, num)
 
+		new = [];
+		array = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+		
+
+		word.split(' ').join('').split('').each do |letter|
+	    i = array.index(letter)
+	    if num > 0
+	    	n = i + num%26
+    	else
+    		n = i - num%26
+	    end
+	    
+	    new << array[n]
+		end
+
+		puts new.join('')
+	end
 end
